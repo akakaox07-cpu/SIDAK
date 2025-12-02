c38c80a2b0206bda42ce00208f61109d2936b556
akakaox07-cpu <akakaox07@gmail.com>
2025-12-03 03:16:37 +0700

    Fix: Import index.css instead of non-existent globals.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/globals.css';
+import './index.css';
 import './styles/print.css';
 
 const rootElement = document.getElementById('root');
