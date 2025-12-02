1f42d28390b80f380d880ec780d9401a21b830ad
akakaox07-cpu <akakaox07@gmail.com>
2025-12-03 03:05:08 +0700

    Remove: PostCSS, Autoprefixer - use Vite native CSS handling

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './styles/globals.css';
 import './styles/print.css';
 
 const rootElement = document.getElementById('root');
